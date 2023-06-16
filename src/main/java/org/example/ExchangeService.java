b60b9c9585ea798e1ef74be11d6834e46e0fc9b3
DerekYook <wowykd@gmail.com>
2023-06-16 18:06:21 +0900

    fix code assignment1

--- src/main/java/org/example/ExchangeService.java
+++ src/main/java/org/example/ExchangeService.java
@@ -5,35 +5,49 @@ import java.util.*;
 public class ExchangeService {
 
     private static Map<String, Integer> productMap = new HashMap<>();
+    private static Map<String, List<String>> storeMap = new HashMap<>();
     private static String productCode;
     private static String storeCode;
     private static Integer stock = 0;
 
-    // 고객 입력
-    private static Scanner scanner = new Scanner(System.in);
-    private static String input = scanner.nextLine();
-    private static final String[] action = input.split(" ");
-    private static String command = action[0].toUpperCase();
-
     public static void main(String[] args) {
-        productMap.put("aaaaaa",10);
+        storeMap.put("aaaaaa", Arrays.asList("012345678", "012345677"));
+        storeMap.put("aaaaab", Arrays.asList("012345678", "012345677", "012345676"));
+        storeMap.put("aaaaaf", Arrays.asList("012345678"));
+        storeMap.put("aaaaag", Arrays.asList("012345678"));
+        storeMap.put("aaaaah", Arrays.asList("012345678"));
+        storeMap.put("aaaaai", Arrays.asList("012345678"));
+        storeMap.put("aaaaaj", Arrays.asList("012345678"));
+        storeMap.put("aaaaaA", Arrays.asList("012345678"));
+
+        productMap.put("012345678", 10);
+        productMap.put("012345677", 20);
+        productMap.put("012345676", 10);
+
+        // 고객 입력
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] action = input.split(" ");
+        String command = action[0].toUpperCase();
+
         if (command.equals("CHECK")) {
             // check : 현재 재고량 확인
             // check + [상품 코드]
-            if (verifyProductCode()) {
+            if (verifyProductCode(action)) {
                 // 재고확인
-                stock = productMap.get(productCode);
+                stock = productMap.get(action[1]);
                 System.out.println("Products remained : " + stock);
+            } else {
+                displayHelp();
             }
-            displayHelp();
         } else if (command.equals("CLAIM")) {
             // claim : 상품 교환
             // claim + [상점 코드] + [상품 코드]
-            if (verifyStoreCode()) {
+            if (verifyStoreCode(action)) {
                 // 상점 재고 확인
-                if (verifyProductCode()) {
+                if (verifyProductCode(action)) {
                     // 재고확인
-                    stock = productMap.get(productCode);
+                    stock = productMap.get(action[2]);
                     System.out.println("Products remained : " + stock);
                 }
                 // case 1 : 차감
@@ -43,8 +57,9 @@ public class ExchangeService {
                 if (stock <= 0) {
                     System.out.println("There is no stock here.");
                 }
+            } else {
+                displayHelp();
             }
-            displayHelp();
         } else {
             displayHelp();
         }
@@ -68,65 +83,104 @@ public class ExchangeService {
                 "2. You should use write 'CLAIM', 'Store Code' and 'Product Code' and there is one space between each of them.");
         System.out.println("3. Product Code has to combine only NUMBERS.");
         System.out.println("  EX1 ) CLAIM asdfgh 123456 => right");
-        System.out.println("  EX2 ) CHECK qwer12 123i1 => wrong");
+        System.out.println("  EX2 ) CLAIM qwer12 123i1 => wrong");
     }
 
     // 상품 코드 유효성 검사
-    public static boolean verifyProductCode() {
-        if (command.equals("CHECK")) {
-            String productCode = action[1];
-            if (productCode.length() != 9) {
-                return false;
-            }
-            for (int i = 0; i < productCode.length(); i++) {
-                try {
-                    int parsedNumber = Integer.parseInt(productCode);
-
-                    if (parsedNumber <= 0) {
-                        return false;
-                    }
-                } catch (NumberFormatException e) {
-                    return false;
-                }
-            }
+    public static boolean verifyProductCode(String[] action) {
+//        public static boolean verifyProductCode() {
+//        if (command.equals("CHECK")) {
+//            String productCode = action[1];
+//            if (productCode.length() != 9) {
+//                return false;
+//            }
+//            for (int i = 0; i < productCode.length(); i++) {
+//                try {
+//                    int parsedNumber = Integer.parseInt(productCode);
+//
+//                    if (parsedNumber <= 0) {
+//                        return false;
+//                    }
+//                } catch (NumberFormatException e) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+        if (action.length < 2) {
+            return false;
+        }
+        String productCode = action[1];
+        if (productCode.length() != 9) {
+            return false;
+        }
+        try {
+            Long.parseLong(productCode);
+        } catch (NumberFormatException e) {
+            return false;
         }
         return true;
     }
 
     // 상점 코드 유효성 검사
-    public static boolean verifyStoreCode() {
-        if (command.equals("CLAIM")) {
-            String storeCode = action[1];
-            if (storeCode.length() != 6) {
+    public static boolean verifyStoreCode(String[] action) {
+//    public static boolean verifyStoreCode() {
+//        if (command.equals("CLAIM")) {
+//            String storeCode = action[1];
+//            if (storeCode.length() != 6) {
+//                return false;
+//            }
+//            for (int i = 0; i < storeCode.length(); i++) {
+//                try {
+//                    if (storeCode.charAt(i) <= 'a' || storeCode.charAt(i) >= 'z') {
+//                        return false;
+//                    } else if (storeCode.charAt(i) <= 'A' || storeCode.charAt(i) >= 'Z') {
+//                        return false;
+//                    }
+//                } catch (Exception e) {
+//                    return false;
+//                }
+//            }
+//            String productCode = action[2];
+//            if (productCode.length() != 9) {
+//                return false;
+//            }
+//            for (int i = 0; i < productCode.length(); i++) {
+//                try {
+//                    int parsedNumber = Integer.parseInt(productCode);
+//
+//                    if (parsedNumber <= 0) {
+//                        return false;
+//                    }
+//                } catch (NumberFormatException e) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+        if (action.length < 3) {
+            return false;
+        }
+        String storeCode = action[1];
+        if (storeCode.length() != 6) {
+            return false;
+        }
+        for (int i = 0; i < storeCode.length(); i++) {
+            char c = storeCode.charAt(i);
+            if (!Character.isLetter(c)) {
                 return false;
             }
-            for (int i = 0; i < storeCode.length(); i++) {
-                try {
-                    if (storeCode.charAt(i) <= 'a' || storeCode.charAt(i) >= 'z') {
-                        return false;
-                    } else if (storeCode.charAt(i) <= 'A' || storeCode.charAt(i) >= 'Z') {
-                        return false;
-                    }
-                } catch (Exception e) {
-                    return false;
-                }
-            }
-            String productCode = action[2];
-            if (productCode.length() != 9) {
-                return false;
-            }
-            for (int i = 0; i < productCode.length(); i++) {
-                try {
-                    int parsedNumber = Integer.parseInt(productCode);
-
-                    if (parsedNumber <= 0) {
-                        return false;
-                    }
-                } catch (NumberFormatException e) {
-                    return false;
-                }
-            }
+        }
+        String productCode = action[2];
+        if (productCode.length() != 9) {
+            return false;
+        }
+        try {
+            Long.parseLong(productCode);
+        } catch (NumberFormatException e) {
+            return false;
         }
         return true;
     }
 }
+
