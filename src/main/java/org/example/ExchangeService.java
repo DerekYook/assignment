e89245f7150cd3593d48aa34582c07cdcf50dd2f
DerekYook <wowykd@gmail.com>
2023-06-20 22:32:21 +0900

    fix code assignment1

--- src/main/java/org/example/ExchangeService.java
+++ src/main/java/org/example/ExchangeService.java
@@ -4,65 +4,79 @@ import java.util.*;
 
 public class ExchangeService {
 
-    private static Map<String, Integer> productMap = new HashMap<>();
-    private static Map<String, List<String>> storeMap = new HashMap<>();
-    private static String productCode;
-    private static String storeCode;
-    private static Integer stock = 0;
-
     public static void main(String[] args) {
-        storeMap.put("aaaaaa", Arrays.asList("012345678", "012345677"));
-        storeMap.put("aaaaab", Arrays.asList("012345678", "012345677", "012345676"));
-        storeMap.put("aaaaaf", Arrays.asList("012345678"));
-        storeMap.put("aaaaag", Arrays.asList("012345678"));
-        storeMap.put("aaaaah", Arrays.asList("012345678"));
-        storeMap.put("aaaaai", Arrays.asList("012345678"));
-        storeMap.put("aaaaaj", Arrays.asList("012345678"));
-        storeMap.put("aaaaaA", Arrays.asList("012345678"));
 
-        productMap.put("012345678", 10);
-        productMap.put("012345677", 20);
-        productMap.put("012345676", 10);
+        Map<String, Integer> productMap = new HashMap<>();
+        Map<String, List<String>> storeMap = new HashMap<>();
+        Integer stock = 0;
+        try {
+            storeMap.put("aaaaaa", Arrays.asList("012345678", "012345677"));
+            storeMap.put("aaaaab", Arrays.asList("012345678", "012345677", "012345676"));
+            storeMap.put("aaaaaf", Arrays.asList("012345678"));
+            storeMap.put("aaaaag", Arrays.asList("012345678"));
+            storeMap.put("aaaaah", Arrays.asList("012345678"));
+            storeMap.put("aaaaai", Arrays.asList("012345678"));
+            storeMap.put("aaaaaj", Arrays.asList("012345678"));
+            storeMap.put("aaaaaA", Arrays.asList("012345678"));
+
+            productMap.put("012345678", 10);
+            productMap.put("012345677", 0);
+            productMap.put("012345676", 20);
+        } catch (Exception e) {
+
+        }
 
         System.out.println("=====ExchangeService=====");
 
         // 고객 입력
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] action = input.split(" ");
-        String command = action[0].toUpperCase();
 
-        if (command.equals("CHECK")) {
-            // check : 현재 재고량 확인
-            // check + [상품 코드]
-            if (verifyProductCode(action)) {
-                // 재고확인
-                stock = productMap.get(action[1]);
-                System.out.println("Products remained : " + stock);
-            } else {
-                displayHelp();
-            }
-        } else if (command.equals("CLAIM")) {
-            // claim : 상품 교환
-            // claim + [상점 코드] + [상품 코드]
-            if (verifyStoreCode(action)) {
-                // 상점 재고 확인
-                stock = productMap.get(action[2]);
+        while(true) {
+            String input = scanner.nextLine();
+            String[] action = input.split(" ");
+            String command = action[0].toUpperCase();
+
+            if (command.equals("CHECK")) {
+                // check : 현재 재고량 확인
+                // check + [상품 코드]
                 if (verifyProductCode(action)) {
+                    String productCode = action[1];
                     // 재고확인
+                    stock = productMap.get(productCode);
                     System.out.println("Products remained : " + stock);
+                } else {
+                    displayHelp();
                 }
-                // case 1 : 차감
-                productMap.put(action[2], stock - 1);
-                // case 2 : 불가 안내
-                if (stock <= 0) {
-                    System.out.println("There is no stock here.");
+            } else if (command.equals("CLAIM")) {
+                // claim : 상품 교환
+                // claim + [상점 코드] + [상품 코드]
+                String productCode = action[2];
+                String storeCode = action[1];
+                if (verifyStoreCode(action)) {
+                    // 상점 재고 확인
+                    stock = productMap.get(productCode);
+                    if (verifyStoreCode(action)) {
+                        // 재고확인
+                        System.out.println("Products remained : " + stock);
+                        // case 1 : 차감
+                        if (stock > 0) {
+                            productMap.put(productCode, stock - 1);
+                            System.out.println("Products remained : " + (stock - 1));
+                        } else if (stock <= 0) {
+                            // case 2 : 불가 안내
+                            System.out.println("There is no stock here.");
+                        } else {
+                            displayHelp();
+                        }
+                    } else {
+                        displayHelp();
+                    }
+                } else {
+                    displayHelp();
                 }
             } else {
                 displayHelp();
             }
-        } else {
-            displayHelp();
         }
     }
 
@@ -89,25 +103,6 @@ public class ExchangeService {
 
     // 상품 코드 유효성 검사
     public static boolean verifyProductCode(String[] action) {
-//        public static boolean verifyProductCode() {
-//        if (command.equals("CHECK")) {
-//            String productCode = action[1];
-//            if (productCode.length() != 9) {
-//                return false;
-//            }
-//            for (int i = 0; i < productCode.length(); i++) {
-//                try {
-//                    int parsedNumber = Integer.parseInt(productCode);
-//
-//                    if (parsedNumber <= 0) {
-//                        return false;
-//                    }
-//                } catch (NumberFormatException e) {
-//                    return false;
-//                }
-//            }
-//        }
-//        return true;
         if (action.length < 2) {
             return false;
         }
@@ -125,40 +120,6 @@ public class ExchangeService {
 
     // 상점 코드 유효성 검사
     public static boolean verifyStoreCode(String[] action) {
-//    public static boolean verifyStoreCode() {
-//        if (command.equals("CLAIM")) {
-//            String storeCode = action[1];
-//            if (storeCode.length() != 6) {
-//                return false;
-//            }
-//            for (int i = 0; i < storeCode.length(); i++) {
-//                try {
-//                    if (storeCode.charAt(i) <= 'a' || storeCode.charAt(i) >= 'z') {
-//                        return false;
-//                    } else if (storeCode.charAt(i) <= 'A' || storeCode.charAt(i) >= 'Z') {
-//                        return false;
-//                    }
-//                } catch (Exception e) {
-//                    return false;
-//                }
-//            }
-//            String productCode = action[2];
-//            if (productCode.length() != 9) {
-//                return false;
-//            }
-//            for (int i = 0; i < productCode.length(); i++) {
-//                try {
-//                    int parsedNumber = Integer.parseInt(productCode);
-//
-//                    if (parsedNumber <= 0) {
-//                        return false;
-//                    }
-//                } catch (NumberFormatException e) {
-//                    return false;
-//                }
-//            }
-//        }
-//        return true;
         if (action.length < 3) {
             return false;
         }
